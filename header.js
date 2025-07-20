0645d12d3f78d1b78d9ac796bd59e77751f022c1
lemanois56 <lemanolivier7@gmail.com>
2025-07-20 17:13:47 +0000

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,1 @@
-document.write('<div class="navbar"><img src="img/bnp-paribas.png" alt="logo_banque" /></div>')
+document.write('<div class="navbar"><img src="https://icons.veryicon.com/png/o/business/bank-logo-collection/standard-chartered-bank-logo.png" alt="logo_banque" /></div>')
